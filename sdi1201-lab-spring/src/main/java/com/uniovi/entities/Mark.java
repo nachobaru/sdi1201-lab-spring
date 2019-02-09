4dfb28086c286ed82d02a50fecfc4eab9ee605aa
nachobaru <ignaciobaruque@gmail.com>
2019-02-09 12:06:56 +0100

    SDI - 1.9 Beans - Servicios, inyección de dependencias

--- sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -4,6 +4,17 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	
+	public Mark(Long id, String description, Double score) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.score = score;
+		}
+		public Mark(){
+		}
+		
+
 
 	public Long getId() {
 		return id;
@@ -27,5 +38,9 @@ public class Mark {
 
 	public void setScore(Double score) {
 		this.score = score;
+	}
+	@Override
+	public String toString() {
+	return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
 	}
 }
