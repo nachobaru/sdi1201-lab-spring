d77d04f478f35c4f7011747f51df3df7f5bf9c89
nachobaru <ignaciobaruque@gmail.com>
2019-02-17 11:01:13 +0100

    SDI-SpringBoot- 4 -Acceso a datos con entidades relacionadas

--- sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -3,25 +3,36 @@ package com.uniovi.entities;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Mark {
-@Id
-@GeneratedValue
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String description;
 	private Double score;
-	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	public Mark(Long id, String description, Double score) {
 		super();
 		this.id = id;
 		this.description = description;
 		this.score = score;
-		}
-		public Mark(){
-		}
-		
+	}
 
+	public Mark() {
+	}
+
+	public Mark(String description, Double score, User user) {
+		super();
+		this.description = description;
+		this.score = score;
+		this.user = user;
+	}
 
 	public Long getId() {
 		return id;
@@ -46,8 +57,17 @@ public class Mark {
 	public void setScore(Double score) {
 		this.score = score;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 	@Override
 	public String toString() {
-	return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
+		return "Mark [id=" + id + ", description=" + description + ", score=" + score + "]";
 	}
 }
