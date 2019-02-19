7e42341dad48b6e39c02b6c2e2d6393fcbeae493
nachobaru <ignaciobaruque@gmail.com>
2019-02-19 14:30:43 +0100

    SDI-SpringBoot-Sesión-4.3-Control de acceso por roles

--- sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
+++ sdi1201-lab-spring/src/main/java/com/uniovi/entities/Mark.java
@@ -13,6 +13,7 @@ public class Mark {
 	private Long id;
 	private String description;
 	private Double score;
+	private Boolean resend = false;
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
@@ -64,6 +65,14 @@ public class Mark {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public Boolean getResend() {
+		return resend;
+	}
+
+	public void setResend(Boolean resend) {
+		this.resend = resend;
 	}
 
 	@Override
