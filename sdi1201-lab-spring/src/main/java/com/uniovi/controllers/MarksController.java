4c6a82211939ad7170b9727d01385ea0937b3c57
nachobaru <ignaciobaruque@gmail.com>
2019-02-19 14:40:33 +0100

    SDI-SpringBoot-Sesión-4.4-Consulta

--- sdi1201-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
+++ sdi1201-lab-spring/src/main/java/com/uniovi/controllers/MarksController.java
@@ -1,8 +1,12 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.services.MarksService;
 import com.uniovi.services.UsersService;
 
@@ -25,10 +30,12 @@ public class MarksController {
 	private UsersService usersService;
 
 	@RequestMapping("/mark/list")
-	public String getList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
-		return "mark/list";
-
+	public String getList(Model model){
+	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	String dni = auth.getName();
+	User user = usersService.getUserByDni(dni);
+	model.addAttribute("markList", marksService.getMarksForUser(user));
+	return "mark/list";
 	}
 
 	@RequestMapping(value = "/mark/add", method = RequestMethod.POST)
@@ -73,8 +80,10 @@ public class MarksController {
 	}
 
 	@RequestMapping("/mark/list/update")
-	public String updateList(Model model) {
-		model.addAttribute("markList", marksService.getMarks());
+	public String updateList(Model model, Principal principal) {
+		String dni = principal.getName(); // DNI es el name de la autenticación
+		User user = usersService.getUserByDni(dni);
+		model.addAttribute("markList", marksService.getMarksForUser(user));
 		return "mark/list :: tableMarks";
 	}
 
--- sdi1201-lab-spring/src/main/java/com/uniovi/services/MarksService.java
+++ sdi1201-lab-spring/src/main/java/com/uniovi/services/MarksService.java
@@ -13,46 +13,60 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.Mark;
+import com.uniovi.entities.User;
 import com.uniovi.repositories.MarksRepository;
 
 @Service
 public class MarksService {
 	@Autowired
 	private HttpSession httpSession;
-	
+
 	@Autowired
-	private MarksRepository marksRepository; 
- 
-	public List<Mark> getMarks(){
+	private MarksRepository marksRepository;
+
+	public List<Mark> getMarks() {
 		List<Mark> marks = new ArrayList<Mark>();
 		marksRepository.findAll().forEach(marks::add);
 		return marks;
-		}
-		public Mark getMark(Long id){
-			Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
-			if ( consultedList == null ) {
+	}
+
+	public Mark getMark(Long id) {
+		Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+		if (consultedList == null) {
 			consultedList = new HashSet<Mark>();
-			}
-			Mark markObtained = marksRepository.findById(id).get();
-			consultedList.add(markObtained);
-			httpSession.setAttribute("consultedList", consultedList);
-			return markObtained;
 		}
-		public void addMark(Mark mark){
+		Mark markObtained = marksRepository.findById(id).get();
+		consultedList.add(markObtained);
+		httpSession.setAttribute("consultedList", consultedList);
+		return markObtained;
+	}
+
+	public void addMark(Mark mark) {
 		// Si en Id es null le asignamos el ultimo + 1 de la lista
 		marksRepository.save(mark);
-		}
-		public void deleteMark(Long id){
-		marksRepository.deleteById(id);
-		}
-		public void setMarkResend(boolean revised,Long id){
-			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-			String dni = auth.getName();
-			Mark mark = marksRepository.findById(id).get();
-			if(mark.getUser().getDni().equals(dni) ) {
-			marksRepository.updateResend(revised, id);
-			}
-			}
-		
+	}
 
+	public void deleteMark(Long id) {
+		marksRepository.deleteById(id);
+	}
+
+	public void setMarkResend(boolean revised, Long id) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String dni = auth.getName();
+		Mark mark = marksRepository.findById(id).get();
+		if (mark.getUser().getDni().equals(dni)) {
+			marksRepository.updateResend(revised, id);
+		}
+	}
+
+	public List<Mark> getMarksForUser(User user) {
+		List<Mark> marks = new ArrayList<Mark>();
+		if (user.getRole().equals("ROLE_STUDENT")) {
+			marks = marksRepository.findAllByUser(user);
+		}
+		if (user.getRole().equals("ROLE_PROFESSOR")) {
+			marks = getMarks();
+		}
+		return marks;
+	}
 }
