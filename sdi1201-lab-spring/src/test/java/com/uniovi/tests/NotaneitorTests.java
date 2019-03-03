b4cf8b19e453963f288267090870c95690de71e3
nachobaru <ignaciobaruque@gmail.com>
2019-03-03 18:03:38 +0100

    SDI-Selenium-5.4-CasosUso-VistaHome

--- sdi1201-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ sdi1201-lab-spring/src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -12,13 +12,16 @@ import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.uniovi.tests.pageobjects.PO_HomeView;
+import com.uniovi.tests.pageobjects.PO_Properties;
+
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NotaneitorTests {
 	// En Windows (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas)):
-	static String PathFirefox64 = "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
-	static String Geckdriver022 = "C:\\Path\\geckodriver024win64.exe";
+	static String PathFirefox64 = "C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe";
+	static String Geckdriver022 = "C:\\Users\\nacar\\Desktop\\SDI\\Practica 5\\PL-SDI-Sesión5-material\\geckodriver022win64.exe";
 	// En MACOSX (Debe ser la versión 65.0.1 y desactivar las actualizacioens
 	// automáticas):
 	// static String PathFirefox65 =
@@ -57,6 +60,29 @@ public class NotaneitorTests {
 	static public void end() {
 		// Cerramos el navegador al finalizar las pruebas
 		driver.quit();
+	}
+	
+	//PR01. Acceder a la página principal /
+	@Test
+	public void PR01() {
+	PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
+	}
+	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+	@Test
+	public void PR02() {
+	PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+	}
+	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+	@Test
+	public void PR03() {
+	PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+	}
+	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+	@Test
+	public void PR04() {
+	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
+	PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
+	//SeleniumUtils.esperarSegundos(driver, 2);
 	}
 
 	@Test
--- sdi1201-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_NavView.java
+++ sdi1201-lab-spring/src/test/java/com/uniovi/tests/pageobjects/PO_NavView.java
@@ -34,14 +34,9 @@ public class PO_NavView extends PO_View {
 		assertTrue(elementos.size() == 1);
 	}
 
-	/**
-	 * Selecciona el enlace de idioma correspondiente al texto textLanguage
-	 * 
-	 * @param driver: apuntando al navegador abierto actualmente.
-	 * @param textLanguage: el texto que aparece en el enlace de idioma ("English" o
-	 *        "Spanish")
-	 */
-	public static void changeIdiom(WebDriver driver, String textLanguage) {// clickamos la opción Idioma.
+	public static void changeIdiom(WebDriver driver, String textLanguage) {
+
+		// clickamos la opción Idioma.
 		List<WebElement> elementos = SeleniumUtils.EsperaCargaPagina(driver, "id", "btnLanguage", getTimeout());
 		elementos.get(0).click();
 		// Esperamos a que aparezca el menú de opciones.
